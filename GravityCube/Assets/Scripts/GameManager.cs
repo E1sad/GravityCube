cf76d965b1b93fdb565e6fee21b1e8a06b8985ec
E1sad <106135426+E1sad@users.noreply.github.com>
2022-06-23 15:43:27 +0400

    RandomLevelGenerate

--- GravityCube/Assets/Scripts/GameManager.cs
+++ GravityCube/Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] PlayerController playerScript;
     [SerializeField] TextMeshProUGUI ScoreText, gameOverScore, gameOverBestScore;
     private float obstacleX = 3f, obstacleY = -0.6f, playerX;
-    private int newScore;
+    private int newScore,randomNum;
     
 
     void Start()
@@ -25,9 +25,13 @@ public class GameManager : MonoBehaviour
     {
         CheckIfGameStart();
         Score();
+        
+    }
+    void Update()
+    {
         CreateRandomObstacle();
     }
-    
+
     void CheckIfGameStart()
     {
         if(Input.GetKeyDown(KeyCode.Space) && playerRb.bodyType == RigidbodyType2D.Static)
@@ -89,7 +93,21 @@ public class GameManager : MonoBehaviour
     //Random Obstacles
     void CreateRandomObstacle()
     {
-        Instantiate(ObstaclePrefab, new Vector2(obstacleX,obstacleY), Quaternion.identity);
-        obstacleX += 6;
+        playerX = player.transform.position.x;
+        if (playerX-obstacleX > -5)
+        {
+            randomNum = Random.Range(0, 2);
+            if (randomNum == 0)
+            {
+                obstacleY = -0.6f;
+                Instantiate(ObstaclePrefab, new Vector2(obstacleX, obstacleY), Quaternion.identity);
+            }
+            if (randomNum == 1)
+            {
+                obstacleY = 0.6f;
+                Instantiate(ObstaclePrefab, new Vector2(obstacleX, obstacleY), Quaternion.Euler(0f,0f,180f));
+            }
+            obstacleX += 7;
+        }
     }
 }
